7d27d81b014c2507b416cbb8008ba016e2f86d53
aadams22 <ashleigh.adams22@gmail.com>
2016-08-26 02:30:56 -0400

    init commit

---
+++ js/index.js
@@ -0,0 +1,19 @@
+// console.log('hi');
+
+$(function(){
+	$("#intro > div > h3").hide();
+
+	var names = $("#intro >div >  h3:nth-of-type(1)");
+	var date = $("#intro > div > h3:nth-of-type(2)");
+
+	function addText(n,d) {
+		n.fadeIn(3000);
+	setTimeout(function(){
+			d.fadeIn(3000);
+		}, 2000);
+	
+	}
+	
+	addText(names, date);
+
+});
